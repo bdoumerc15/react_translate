b2795145c642741d6a7b7d7d2250a1fdf1d4181a
bladimir doumerc <bdoumerc15@gmail.com>
2021-02-25 21:49:21 -0700

    Se agregan nuevos archivos y se termina el mini proyecto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Field from "./components/field"
+import Languages from "./components/languages"
+import Translate from "./components/translate"
+
+export default function App() {
+    const [language, setLanguage] = React.useState("ru");
+    const [text, setText] = React.useState("");
+    return (
+        <div>
+            <Field label="Enter English" onChange={setText} value={text} />
+            <Languages language={language} onLanguageChange={setLanguage} />
+            <hr />
+            <Translate text={text} language={language} />
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-const rootElement = document.querySelector("#root");
 
 ReactDOM.render(<App />, document.getElementById("root"));
